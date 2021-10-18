f7f6a683a1cca53b4a2538977717427c3a97d85d
Luiz Amorim <luiz.simples@gmail.com>
2021-10-18 11:14:17 -0400

    deve retornar a palavra brio

--- src/anagrama.js
+++ src/anagrama.js
@@ -1,4 +1,31 @@
 module.exports = (word) => {
+  const len = word.length
+  const words = []
 
-  return [word, 'bior']
+  for (let index = 0; index < len; index++) {
+    words.push(...getPossibilities(word, index))
+  }
+
+  return words
+}
+
+
+const getPossibilities = (word, index) => {
+  const firstChar = word[index]
+  console.log({firstChar, index, word})
+  const chars = word.split('')
+  chars.splice(index, 1)
+  const newWord = chars.join('')
+  const len = newWord.length
+  if (!len) return []
+
+  const words = [word]
+
+  for (let i = 0; i < len; i++) {
+    const possibilities = getPossibilities(newWord, i)
+    console.log(possibilities)
+    words.push(...possibilities.map((word) => `${firstChar}${word}`))
+  }
+
+  return words
 }
--- tests/src/sample.test.js
+++ tests/src/sample.test.js
@@ -4,8 +4,12 @@ it('Deve retornar a palavra biro', () => {
   expect(anagrama('biro')).toContain('biro');
 });
 
-it('Deve retornar a palavra biro', () => {
+it('Deve retornar a palavra bior', () => {
   expect(anagrama('biro')).toContain('bior');
+});
+
+it('Deve retornar a palavra brio', () => {
+  expect(anagrama('biro')).toContain('brio');
 });
 
 
